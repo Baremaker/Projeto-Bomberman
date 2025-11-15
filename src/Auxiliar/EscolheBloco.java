aef01bf5e4c9739f146912dd629a916b503ef7db
aserr <aserr@alien_x>
2025-11-15 20:00:14 -0300

    Agora a explosão é validada pela propria classe explosão e a logica para dar dano e destruir objetos ocorre durante essa validação

--- src/Auxiliar/EscolheBloco.java
+++ src/Auxiliar/EscolheBloco.java
@@ -2,19 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Modelo;
-
-import java.io.Serializable;
+package Auxiliar;
 
 /**
  *
  * @author aserr
  */
-public class Powerup extends Model implements Serializable {
-
-    public Powerup(String sNomeImagePNG, int linha, int coluna) {
-        super(sNomeImagePNG, linha, coluna);
-        
-    }
+public class EscolheBloco {
+      
+    
+    
     
 }
